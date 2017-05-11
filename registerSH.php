6250f13994c7b6acb94ef2eae06c234a1ebed92c
Simon Karlsson <Simonk1994@hotmail.com>
2017-05-11 13:37:07 +0200

    ny register

--- registerSH.php
+++ registerSH.php
@@ -6,9 +6,8 @@ $db = "johnie16_db";
 $mysqli = new mysqli($host, $user, $pwd, $db);
 if (isset($_POST['username']) and isset($_POST['password'])) {
 $query = <<<END
-INSERT INTO users(username,password,email,fname,lname)
-VALUES('{$_POST['username']}','{$_POST['password']}','
-{$_POST['email']}','{$_POST['fname']}','{$_POST['lname']}')
+INSERT INTO smartusers(username,password)
+VALUES('{$_POST['username']}','{$_POST['password']}')
 END;
 if ($mysqli->query($query) !== TRUE) {
 die("Could not query database" . $mysqli->errno . " : " . $mysqli->error);
@@ -19,9 +18,6 @@ $content = <<<END
 <form method="post" action="registerSH.php">
 <input type = "text" name="username" placeholder="username" ><br>
 <input type="password" name="password" placeholder="password"><br>
-<input type="text" name="email" placeholder="email"><br>
-<input type="text" name="fname" placeholder="first name"><br>
-<input type="text" name="lname" placeholder="last name"><br>
 <input type="submit" value="Register">
 <input type="Reset" value="reset">
 </form>
