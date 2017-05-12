f575d7d2dd1f7033ab41f09fdddc92f2b9aa1387
fluggah <simonk1994@hotmail.com>
2017-05-12 16:05:44 +0200

    php change

---
+++ template.php
@@ -0,0 +1,453 @@
+<!DOCTYPE html>
+<html>
+<title>Smart Home Application</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles/styles.css">
+<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+<link rel="stylesheet" href="emesmakeover.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/
+font-awesome.min.css">
+
+<style>
+html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
+</style>
+<body class="w3-theme-l5">
+
+<!-- Navbar -->
+<div class="w3-top">
+ <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
+  <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+  <a href="template.html" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Home</a>
+
+  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account"><img src="max.png" class="w3-circle" style="height:25px;width:25px" alt="Avatar"> Log out</a>
+ </div>
+</div>
+
+<!-- Navbar on small screens -->
+
+
+<!-- Page Container -->
+<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">    
+  <!-- The Grid -->
+  <div class="w3-row">
+
+
+      <!-- Left Column -->
+    <div style="padding-right:1%; padding-left:1%" class="w3-col m7">
+
+
+       <div class="w3-card-2 w3-round w3-white w3-hide-small">
+        <div class="w3-container">
+          <h1>Bathroom</h1>
+  
+
+
+
+          Fan <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+
+          Lamp <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>   
+
+          Air C <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a> 
+
+          
+
+
+<!--<div style="width:15%" class="dropdown">
+          <button onclick="kitchenFunction()" class="dropbtn">Kitchen</button>
+          <div id="kitchenDropdown" class="dropdown-content">
+          <a href="#">Fridge <label style="left:40%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Freezer <label style="left:36%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Oven <label style="left:45%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+  </div>
+
+</div> -->
+
+<!--<div style="width:15%" class="dropdown">
+          <button onclick="bedroomFunction()" class="dropbtn">Bedroom</button>
+          <div id="bedroomDropdown" class="dropdown-content">
+          <a href="#">Table Lamp <label style="left:20%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Air-Condition <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+        
+  </div>
+
+</div> -->
+
+
+        </div>
+        <br>
+      </div>
+
+
+
+
+
+      <br>
+
+
+
+
+
+
+
+      <div class="w3-card-2 w3-round w3-white w3-hide-small">
+        <div class="w3-container">
+          <h1>Kitchen</h1>
+       <!-- <div class="dropdown">
+          <button onclick="livingroomFunction()" class="dropbtn">Livingroom</button>
+          <div id="livingroomDropdown" class="dropdown-content">
+          <a href="#">TV <label style="left:50%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Table Lamp<label style="left:22%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+  </div>
+
+</div>     -->
+
+
+
+
+            Fridge <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a> 
+
+          Freezer <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a> 
+
+           Owen <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a> 
+
+<!--<div style="width:15%" class="dropdown">
+          <button onclick="bedroomFunction()" class="dropbtn">Bedroom</button>
+          <div id="bedroomDropdown" class="dropdown-content">
+          <a href="#">Table Lamp <label style="left:20%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Air-Condition <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+        
+  </div>
+
+</div> -->
+
+
+        </div>
+        <br>
+      </div>
+
+
+
+
+
+  
+
+
+    
+    <!-- End Left Column -->
+    </div>
+
+    
+    <!-- Middle Column -->
+    <div style="padding-right:1%; padding-left:1%" class="w3-col m7">
+    
+
+       <div class="w3-card-2 w3-round w3-white w3-hide-small">
+        <div class="w3-container">
+
+          <h1>Livingroom</h1>
+
+
+
+          TV <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+
+          Lamp <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>   
+
+          Air C <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a> 
+
+
+          
+<!--<div style="width:15%" class="dropdown">
+          <button onclick="kitchenFunction()" class="dropbtn">Kitchen</button>
+          <div id="kitchenDropdown" class="dropdown-content">
+          <a href="#">Fridge <label style="left:40%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Freezer <label style="left:36%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Oven <label style="left:45%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+  </div>
+
+</div> -->
+
+<!--<div style="width:15%" class="dropdown">
+          <button onclick="bedroomFunction()" class="dropbtn">Bedroom</button>
+          <div id="bedroomDropdown" class="dropdown-content">
+          <a href="#">Table Lamp <label style="left:20%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Air-Condition <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+        
+  </div>
+
+</div> -->
+
+
+        </div>
+        <br>
+      </div>
+
+
+
+
+
+      <br>
+
+
+
+
+
+
+
+      <div class="w3-card-2 w3-round w3-white w3-hide-small">
+        <div class="w3-container">
+          <h1>Bedroom</h1>
+          <!--<div class="dropdown">
+          <button onclick="livingroomFunction()" class="dropbtn">Livingroom</button>
+          <div id="livingroomDropdown" class="dropdown-content">
+          <a href="#">TV <label style="left:50%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Table Lamp<label style="left:22%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+  </div>
+
+</div>  -->   
+
+<!--<div style="width:15%" class="dropdown">
+          <button onclick="kitchenFunction()" class="dropbtn">Kitchen</button>
+          <div id="kitchenDropdown" class="dropdown-content">
+          <a href="#">Fridge <label style="left:40%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Freezer <label style="left:36%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Oven <label style="left:45%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+  </div>
+
+</div> -->
+
+<div>
+        <!--  <div id="bedroomDropdown" class="dropdown-content">-->
+          Lamp <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+
+          Air.C <a href="#"><label class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+
+        
+          <!--<a href="#">Ceiling Lamp <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>
+          <a href="#">Air-Condition <label style="left:15%; top:8px" class="switch">
+          <input type="checkbox">
+          <div class="slider round"></div>
+          </label></a>-->
+        
+
+
+
+        </div>
+       <br>
+      </div>
+</div>
+   
+      
+    <!-- End Middle Column -->
+    </div>
+
+
+
+
+
+
+    <!-- Right Column -->
+        <div style="padding-right:1%; padding-left:1%" class="w3-col m7">
+
+ 
+     
+
+
+      <div class="w3-card-2 w3-round w3-white">
+        <div class="w3-container">
+         <h4 class="w3-center">Group 14</h4>
+         <p class="w3-center"><img src="max.png" class="w3-circle" style="height:106px;width:106px" alt="Avatar"></p>
+         <hr>
+         <p><i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i> Smart Home Application</p>
+         <p><i class="fa fa-home fa-fw w3-margin-right w3-text-theme"></i> Halmstad, Sweden</p>
+         
+        </div>
+      </div>
+      
+      
+    <!-- End Right Column -->
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  <!-- End Grid -->
+  </div>
+  
+<!-- End Page Container -->
+</div>
+<br>
+
+<!-- Footer -->
+<footer class="w3-container w3-theme-d3 w3-padding-16">
+  <h5>Grupp 14</h5>
+</footer>
+
+<script type="text/javascript" src="jscript/jscript1.js"></script> 
+<script>
+// Accordion
+
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-theme-d1";
+    } else { 
+        x.className = x.className.replace("w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-theme-d1", "");
+    }
+}
+
+// Used to toggle the menu on smaller screens when clicking on the menu button
+function openNav() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+</script>
+
+</body>
+</html> 
+
